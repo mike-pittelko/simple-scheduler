195b3d38ecbe910562b86c639bcedaabbe1a53be
Michael Pittelko <mike@pittelko.net>
2020-02-22 15:13:24 -0600

    Added a slight-less-simple-scheduler for running from main loop.

---
+++ examples/slightly-less-simple-scheduler/stm32cube/task-schedule.c
@@ -0,0 +1,132 @@
+/*
+ *
+ *
+  task-schedule.c
+
+	Implements a very simple task scheduler, based on input of a count of milliseconds
+
+  Copyright (c) 2020 Mike Pittelko
+
+  Grbl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Grbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include "task-schedule.h"
+
+static const int MultiplyDeBruijnBitPosition[32] =
+{
+  0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
+  31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
+};
+
+uint32_t Scheduler_CountOfZeros = 0;
+
+
+// Returns number of trailing zeros in 5 operations (that doesn't necessarily mean 5 isntructions).
+// I did say it was black magic.
+// "Using de Bruijn Sequences to Index 1 in a Computer Word" by Charles E. Leiserson, Harald Prokof, and Keith H. Randall
+
+uint16_t CountZerosFromLSB(uint32_t v)
+{
+	return  MultiplyDeBruijnBitPosition[((uint32_t)((v & -v) * 0x077CB531U)) >> 27];
+}
+
+
+void (*Tasks[]) (void) = {
+		0,			       // Never called
+		TASK_2MS,          // Bit 1
+		TASK_4MS,          // Bit 2
+		TASK_8MS, 	       // Bit 3
+		TASK_16MS,         // Bit 4
+		TASK_32MS,         // Bit 5
+		TASK_64MS,         // Bit 6
+		TASK_128MS,        // Bit 7
+		TASK_2562MS,	   // Bit 8
+		TASK_512MS,	       // Bit 9
+		TASK_1024MS,	   // Bit 10
+		TASK_2048MS,	   // Bit 11
+		TASK_4096MS,	   // Bit 12
+		TASK_8192MS,	   // Bit 13
+		TASK_16384MS,	   // Bit 14
+		TASK_32768MS,	   // Bit 15
+		TASK_65536MS,	   // Bit 16
+		TASK_131072MS,	   // Bit 17
+		TASK_262144MS,	   // Bit 18
+		TASK_528288MS,	   // Bit 19
+		TASK_1056576MS,    // Bit 20
+		TASK_2113152MS,    // Bit 21
+		TASK_4226304MS,    // Bit 22
+		TASK_8452608MS,    // Bit 23
+		TASK_16905216MS,   // Bit 24
+		TASK_33810432MS,   // Bit 25
+		TASK_67620864MS,   // Bit 26
+		TASK_135241728MS   // Bit 27.  Can be extended to all 32 bits (or more), but for
+		                   //          general purposes, 38 days is probably sufficient.
+
+};
+
+
+// Used to ensure the top bits we won't use are always 1
+#define TASK_MASK 0xF0000000
+
+
+uint32_t stm32_atomic_swap(uint32_t *dest, uint32_t new_value)
+{
+  uint32_t old_value;
+  do
+  {
+    old_value = __LDREXW(dest);
+  } while (__STREXW(new_value,dest));
+  return old_value;
+}
+
+
+// Given time in ms, compute the number of zeros given the current time.
+// Happens in interrupt context.
+void TaskSignals(uint32_t TimeMs)
+{
+	Scheduler_CountOfZeros = CountZerosFromLSB((TimeMs|TASK_MASK));  // Note, uint32_t is inherently atomic on arm
+}
+
+// call tasks on (potentially sloppy) powers of
+// 2 ms (2,4,8,16,32,64,128,256,512,1024,2048,4096...)
+// On a given slot, will call the maximum slot, then in descending order,
+// For example, at 16ms, the 16ms slot, the 8ms slot, the 4ms slot, and 2ms slot will be called in
+// that order.
+// THIS IS A COOPERATIVE SCHEDULER, NOT A PREEMTIVE ONE.
+// Returns number tasks that were called.
+
+uint32_t MainLoopTaskProcessor(void)
+{
+	uint32_t CountOfZeros = 0;
+	uint32_t TasksThatRan = 0;
+
+	CountOfZeros = stm32_atomic_swap(&Scheduler_CountOfZeros,0);  // By clearing the count, if function is called
+															// before we get the next tick, will keep from
+															// running tasks again.
+
+	if ((CountOfZeros != 0) & (CountOfZeros <= 27))	// Safety - the task signals TASK_MASK is set for 28 bits., and don't bother going further if count == 0
+		{
+			while (CountOfZeros != 0)		  // Only process if at least 1 zero exists
+			{
+				if (Tasks[CountOfZeros] != 0) // If a task is defined
+				{
+					TasksThatRan++;
+					Tasks[CountOfZeros]();	  // Call the task
+				}
+				CountOfZeros--;				  // Decrease the index
+			}
+		}
+	return TasksThatRan;
+}
---
+++ slightly-less-simple-scheduler/task-schedule.h
@@ -0,0 +1,130 @@
+/*
+ *
+ *
+  task-schedule.h
+
+	Implements a very simple task scheduler, based on input of a count of milliseconds
+
+  Copyright (c) 2020 Mike Pittelko
+
+  Grbl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Grbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+#ifndef __task_schedule_h
+#define __task_schedule_h
+
+#include "utilities.h"
+#include "main.h"
+
+/* Prototypes for tasks
+ *
+ * There may be at most one function called for each time slot. The function you
+ * define can call as many other functions as you like, but be cognizant of the
+ * amount of time consumed by each.  For example: calling a functions that takes
+ * 32ms to execute every 32ms will not leave any time for the 2ms, 4ms, 8ms, 16ms
+ * tasks to execute without causing time to "slip" and tasks *skipped* the next time
+ * the scheduler runs to catch up with the time pattern.
+ *
+ * On a given slot, will call the maximum time, then each match in descending order,
+ * For example, at 16ms: the 16ms slot, the 8ms slot, the 4ms slot, and 2ms slot will
+ * be called in that order.
+ *
+ *
+
+void Task2ms(void) { };			// Called every 2ms
+void Task4ms(void) { };			// Called every 4ms
+void Task8ms(void) { };			// Called every 8ms
+void Task16ms(void) { };		// Called every 16ms
+void Task32ms(void) { };		// Called every 32ms
+void Task64ms(void) { };		// Called every 64ms
+void Task128ms(void) { };		// Called every 128ms
+void Task256ms(void) { };		// Called every 256ms
+void Task512ms(void) { };		// Called every 512ms
+void Task1024ms(void) { };		// Called every 1.024s
+void Task2048ms(void) { };		// Called every 2.048s
+void Task4096ms(void) { };		// Called every 4.096s
+void Task8192ms(void) { };		// Called every 8.192s
+void Task16384ms(void) { };		// Called every 16.384s
+void Task32768ms(void) { };		// Called every 32.768s
+void Task65536ms(void) { };		// Called every 65.536s, About 1 minute
+void Task131072ms(void) { };	// Called every 131.072s, About 2 minutes
+void Task262144ms(void) { };	// Called every 262.144s, About 4 minutes
+void Task528288ms(void) { };	// Called every 524.288s, About 9 minutes
+void Task1056576ms(void) { };	// Called every 1056.576s, About 17 minutes
+void Task2113152ms(void) { };	// Called every 2113.152s, About 35 minutes
+void Task4226304ms(void) { };	// Called every 4226.304s, About 70 minutes
+void Task8452608ms(void) { };	// Called every 8452.608s, About 140 minutes, About 2.3 hours
+void Task16905216ms(void) { };	// Called every 16905.216s, About 281 minutes, About 4.7 hours
+void Task33810432ms(void) { };	// Called every 33810.432s, About 563 minutes, About 9.4 hours
+void Task67620864ms(void) { };	// Called every 67620.864s, About 1127 minutes, About 18.8 hours
+void Task135241728ms(void) { };	// Called every 135241.728s, About 2254 minutes, About 37.6 hours
+
+*/
+
+
+// For each definition of a task, install your function pointer
+// #define EXAMPLE_TASK_DEFINE (Task2ms) // call void Task2ms(void); every 2ms
+// #define EXAMPLE_TASK_NOT_USED (0)   // Unused task slots MUST BE ZERO
+#define TASK_2MS 		 (0)
+#define TASK_4MS 		 (0)
+#define TASK_8MS 		 (0)
+#define TASK_16MS 		 (0)
+#define TASK_32MS 		 (0)
+#define TASK_64MS 		 (0)
+#define TASK_128MS 		 (0)
+#define TASK_2562MS 	 (Task256ms)
+#define TASK_512MS 		 (0)
+#define TASK_1024MS 	 (Task1024ms)
+#define TASK_2048MS 	 (0)
+#define TASK_4096MS 	 (0)
+#define TASK_8192MS 	 (0)
+#define TASK_16384MS 	 (0)
+#define TASK_32768MS 	 (0)
+#define TASK_65536MS 	 (0)
+#define TASK_131072MS 	 (0)
+#define TASK_262144MS 	 (0)
+#define TASK_528288MS 	 (0)
+#define TASK_1056576MS 	 (0)
+#define TASK_2113152MS 	 (0)
+#define TASK_4226304MS 	 (0)
+#define TASK_8452608MS 	 (0)
+#define TASK_16905216MS  (0)
+#define TASK_33810432MS  (0)
+#define TASK_67620864MS  (0)
+#define TASK_135241728MS (0)
+
+
+// Given time in ms, set up scheduler for tasks to run.  Call this from tick handler.
+void TaskSignals(uint32_t TimeMs);
+
+
+//call tasks on powers of 2 ms (2,4,8,16,32,64,128,256,512,1024,2048,4096...)
+// On a given slot, will call the maximum time, then in descending order,
+// For example, at 16ms, the 16ms slot, the 8ms slot, the 4ms slot, and 2ms slot will be called in
+// that order.
+// Be aware that if it takes longer to run the given tasks that the tick increment that TASKS WILL BE SKIPPED
+// to "catch up" the time.
+// Call this from your main loop
+// Returns number tasks that were called.
+
+uint32_t MainLoopTaskProcessor(void);
+
+// From the LSB, count the number of zero's using black magic.
+uint16_t CountZerosFromLSB(uint32_t v);
+
+
+
+#endif // __task_schedule_h
